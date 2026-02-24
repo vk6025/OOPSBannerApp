d41045bbf5111b86dba7167c050775ed736175d9
vk6025 <vk6025@srmist.edu.in>
2026-02-24 10:32:10 +0530

    Add UC2 class to print a pattern

---
+++ uc_2.java
@@ -0,0 +1,11 @@
+class UC2 {
+    public static void main(String args[]){
+        System.out.println("**	**	**	**");
+		System.out.println("*  *	*  *	*  *	*");
+		System.out.println("*  *	*  *	*  *	*");
+		System.out.println("*  *	*  *	**	**");
+		System.out.println("*  *	*  *	*			*");
+		System.out.println("*  *	*  *	*			*");
+		System.out.println("**	**	*		***");
+    }
+}
