2bcef0a0420120a972f8ebbebfe65b2a14b84728
vk6025 <vk6025@srmist.edu.in>
2026-02-24 10:33:10 +0530

    Add uc3.java to print a star pattern

---
+++ uc_3.java
@@ -0,0 +1,12 @@
+class uc3 {
+    public static void main(String args[]) {
+
+        System.out.println(String.join("   ", "  * "," ", "  * "," ", "  * "," ", " * "));
+        System.out.println(String.join("   ", "*  "," ", "  "," ", "  "," ", "*    "));
+        System.out.println(String.join("   ", "*  "," ", "  "," ", "*  "," ", "**    "));
+        System.out.println(String.join("   ", "*  "," ", "  "," ", "    "," ", " **  "));
+        System.out.println(String.join("   ", "*  "," ", "  "," ", "    "," ", "    *"));
+        System.out.println(String.join("   ", "*  "," ", "  "," ", "    "," ", "    *"));
+        System.out.println(String.join("   ", " * "," ", "  * "," ", " *    "," ", " * "));
+    }
+}
